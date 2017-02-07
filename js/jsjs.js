dc3cb5e66980c0532f49c95e92fec4d2f48b527c
ginobilee <ginobilee@outlook.com>
2017-02-07 21:13:09 +0800

    add files

---
+++ js/jsjs.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('myApp',[]);
+
+app.controller('myCtrl',function($scope,$interval){
+	$scope.pause = true;
+	$scope.timeGone = 0-8*60*60*1000;
+	
+
+	$scope.runTimer = function(){	
+					$scope.pause = !$scope.pause;
+					
+					if($scope.pause){//暂停
+						$interval.cancel($scope.timeoutId);
+					}else{
+						$scope.timeoutId = $interval(function(){
+								$scope.timeGone = $scope.timeGone + 1000;
+						},1000);
+					}
+	}	
+								
+});
